39e7b40c8bfdcef9841eb42290a2753310722265
LiangGuanYu <153156766@qq.com>
2022-11-16 01:38:41 +0000

    defer rendering

--- Blank Project/Renderer.h
+++ Blank Project/Renderer.h
@@ -16,20 +16,41 @@ protected:
 	void DrawWater();
 	void DrawSkybox();
 
+	void FillBuffers();
+	void DrawPointLights();
+	void CombineBuffers();
+
+	void GenerateScreenTexture(GLuint& into, bool depth = false);
+
+	Shader* sceneShader; 
+	Shader * pointlightShader; 
+	Shader * combineShader; 
+
 	Shader* lightShader;
 	Shader* reflectShader;
 	Shader* skyboxShader;
 
 	HeightMap* heightMap;
 	Mesh* quad;
-
+	Mesh* quad2;
 	Light* light;
 	Camera* camera;
+	Light* pointLights; // Array of lighting data
+	Mesh * sphere;	// Light volum
 
 	GLuint cubeMap;
 	GLuint waterTex;
 	GLuint earthTex;
 	GLuint earthBump;
+
+	GLuint bufferFBO; // FBO for our G- Buffer pass
+	GLuint bufferColourTex; // Albedo goes here
+	GLuint bufferNormalTex; // Normals go here
+	GLuint bufferDepthTex; // Depth goes here
+	GLuint pointLightFBO; // FBO for our lighting pass
+	GLuint lightDiffuseTex; // Store diffuse lighting
+	GLuint lightSpecularTex; // Store specular lighting
+
 
 	float waterRotate;
 	float waterCycle;
